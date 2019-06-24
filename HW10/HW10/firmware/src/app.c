0becc2bc875ebc8b0d4c3e9df92afb62ddbe9f3b
dhanvink <46697688+dhanvin-k@users.noreply.github.com>
2019-06-24 00:10:23 -0500

    Update app.c

--- HW10/HW10/firmware/src/app.c
+++ HW10/HW10/firmware/src/app.c
@@ -336,17 +336,17 @@ void APP_Initialize(void) {
     
     __builtin_disable_interrupts();
 
-    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
-    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
-
-    // 0 data RAM access wait states
-    BMXCONbits.BMXWSDRM = 0x0;
-
-    // enable multi vector interrupts
-    INTCONbits.MVEC = 0x1;
-
-    // disable JTAG to get pins back
-    DDPCONbits.JTAGEN = 0;
+//    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+//    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+//
+//    // 0 data RAM access wait states
+//    BMXCONbits.BMXWSDRM = 0x0;
+//
+//    // enable multi vector interrupts
+//    INTCONbits.MVEC = 0x1;
+//
+//    // disable JTAG to get pins back
+//    DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
     TRISBbits.TRISB4 = 1;
