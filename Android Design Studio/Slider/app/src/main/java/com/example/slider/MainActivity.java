8340708432bc588fd52d2a1b7eba6aa637a46429
dhanvink <46697688+dhanvin-k@users.noreply.github.com>
2019-09-19 15:44:01 -0500

    created slider app

---
+++ Android Design Studio/Slider/app/src/main/java/com/example/slider/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.slider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar myControl;
+    TextView myTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myControl = (SeekBar) findViewById(R.id.seek1);
+
+        myTextView = (TextView) findViewById(R.id.textView01);
+        myTextView.setText("Enter whatever you Like!");
+
+        setMyControlListener();
+    }
+
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                myTextView.setText("The value is: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
