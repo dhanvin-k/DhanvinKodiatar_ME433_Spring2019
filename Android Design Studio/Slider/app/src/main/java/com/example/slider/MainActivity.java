0a6a066666186091d1384c46a343ad71377cd127
dhanvink <46697688+dhanvin-k@users.noreply.github.com>
2019-09-19 15:57:20 -0500

    implemented slider

--- Android Design Studio/Slider/app/src/main/java/com/example/slider/MainActivity.java
+++ Android Design Studio/Slider/app/src/main/java/com/example/slider/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.slider;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     SeekBar myControl;
     TextView myTextView;
 
+    // global variables for the widgets:
+    Button button;
+    TextView myTextView2;
+    ScrollView myScrollView;
+    TextView myTextView3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
 
         myTextView = (TextView) findViewById(R.id.textView01);
         myTextView.setText("Enter whatever you Like!");
+
+        // create an event to detect when the button is pressed that writes the slider position to the textview:
+        myTextView2 = (TextView) findViewById(R.id.textView02);
+        myScrollView = (ScrollView) findViewById(R.id.ScrollView01);
+        myTextView3 = (TextView) findViewById(R.id.textView03);
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myTextView2.setText("value on click is "+myControl.getProgress());
+            }
+        });
 
         setMyControlListener();
     }
